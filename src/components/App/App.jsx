a3c3152054f249424f04bb99033d6488230c03ea
Yuriy-St <yuriystaynov@gmail.com>
2023-08-13 15:23:33 +0300

    step 2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -16,15 +16,30 @@ export default class App extends Component {
       return { [value]: prevState[value] + 1 };
     });
 
+  countTotalFeedback() {
+    const { good, bad, neutral } = this.state;
+    return good + bad + neutral;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const total = this.countTotalFeedback();
+    return total ? Math.round((this.state.good / total) * 100) : 0;
+  }
+
   render() {
+    const { state } = this;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
     return (
       <AppContainer>
         <Subtitle>Please leave feedback</Subtitle>
         <FeedbackControls riseValue={this.riseValue} />
         <Subtitle>Statistics</Subtitle>
-        <Line>Good: {this.state.good}</Line>
-        <Line>Neutral: {this.state.neutral}</Line>
-        <Line>Bad: {this.state.bad}</Line>
+        <Line>Good: {state.good}</Line>
+        <Line>Neutral: {state.neutral}</Line>
+        <Line>Bad: {state.bad}</Line>
+        <Line>Total: {total}</Line>
+        <Line>Positive feedback: {positivePercentage}%</Line>
       </AppContainer>
     );
   }
